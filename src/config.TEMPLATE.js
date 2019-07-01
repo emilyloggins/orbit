edfb42281653cce35960ab560c8a46c49c114af9
Emily Loggins <em.loggins7@gmail.com>
2019-07-01 11:54:10 -0500

    config/key templates

---
+++ src/config.TEMPLATE.js
@@ -0,0 +1,16 @@
+//Replace the values with your information and rename this file to config.js
+//Don't forget to make sure this file is included in your .gitignore
+
+export const FBApiKey = "yourKeyHere"
+
+export const FBAuthDomain = "yourKeyHere"
+
+export const FBDatabaseURL = "yourKeyHere"
+
+export const FBProjectID = "yourKeyHere"
+
+export const FBStorageBucket = "yourKeyHere"
+
+export const FBMessageSenderId = "yourKeyHere"
+
+export const FBAppID = "yourKeyHere"
